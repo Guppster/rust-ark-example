1c1ad6cd7c67624e1d7a8c7562e06af6d4348802
Ian <ian@andrews-mbp.lan>
2017-10-08 17:45:34 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# rust-ark-example
+
+<img src="ark_example.png"  align="center">
+
+This an example project demonstrating the Rust Ark. To see it in action, download the source and run:
+
+```rust
+cargo run
+```
---
+++ src/main.rs
@@ -0,0 +1,48 @@
+extern crate ark;
+extern crate term_painter;
+
+use term_painter::{ToStyle, Color};
+use term_painter::Color::*;
+use term_painter::Attr::*;
+
+use ark::manager;
+use ark::models;
+
+
+fn main() { 
+
+    println!("{}",
+    BrightMagenta.bold().paint("
+ ########   ##     ##   ######   ########        ###     ########   ##    ##  
+ ##     ##  ##     ##  ##    ##     ##          ## ##    ##     ##  ##   ##  
+ ##     ##  ##     ##  ##           ##         ##   ##   ##     ##  ##  ##   
+ ########   ##     ##   ######      ##        ##     ##  ########   #####    
+ ##   ##    ##     ##        ##     ##        #########  ##   ##    ##  ##   
+ ##    ##   ##     ##  ##    ##     ##        ##     ##  ##    ##   ##   ##  
+ ##     ##   #######    ######      ##        ##     ##  ##     ##  ##    ##")
+        );
+
+    println!("");
+    println!("{}", Cyan.bold().paint("Fetching newests posts from ArkCoin..."));
+    println!("");
+
+    let manager = manager::Manager::default();
+
+    let t = manager.get_received_transactions("AZHXnQAYajd3XkxwwiL6jnLjtDHjtAATtR");
+
+    match t {
+    Ok(response) => print_transaction(response.transactions),
+    Err(e) => println!("Error: {:?}", e),
+}
+
+fn print_transaction(transactions: Option<Vec<models::Transaction>>) {
+    if let Some(mut unwrapped_transactions) = transactions {
+        for entry in unwrapped_transactions.iter_mut() {
+            if let Some(ref vendor_field) = entry.vendorField {
+                println!("{}", Green.bold().paint(vendor_field));
+                println!("");
+            }
+        }
+    }
+}
+}
